c0fb8cf26c858b7213e0f5aff34b4aea42cdf9be
nikhil <dendetinikhil@hmail.com>
2025-06-20 12:39:20 +0530

    CaseType description removed

--- users/models.py
+++ users/models.py
@@ -35,7 +35,6 @@ class CaseType(models.Model):
 
     case_type_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=30, choices=CASE_TYPE_CHOICES, unique=True)
-    description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.get_name_display()
