23b646b1c63ac04f646b5efe3746519ed5260265
nikhil <dendetinikhil@hmail.com>
2025-06-20 12:37:36 +0530

    CaseType updated

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import User, CaseType, Complaint, AIAssessment, Accused, Evidence, Witness, ChatQuery
-# Register your models here.
+
 admin.site.register(User)
 admin.site.register(CaseType)
 admin.site.register(Complaint)
